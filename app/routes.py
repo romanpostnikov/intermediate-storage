40c351a2d10b4078b3a186c57627bbd3f35d92fb
romanpostnikov <postnikovrom@gmail.com>
2020-03-20 15:35:40 +0200

    changed return of routes

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
-from app import app
+from app import app, db
+from app.models import Video
+from flask import request
+import os
+import googleapiclient.discovery
+import json
 
 @app.route('/')
 def index():
-	return "<h1>Updated world!</h1>"
+	return "Updated world"
